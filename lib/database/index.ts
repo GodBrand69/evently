3c6e520b54d83ab1cbc2c850b1eee61d40bc05f1
godbrand69 <you@example.com>
2023-12-25 18:04:04 +0530

    made some new changes

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null }
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connectToDatabase =async () => {
+export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
 
-    if(!MONGODB_URL) throw new Error("MONGODB URL is missing")
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName: 'evently',
-        bufferCommands: false
+        bufferCommands: false,
     })
 
     cached.conn = await cached.promise;
